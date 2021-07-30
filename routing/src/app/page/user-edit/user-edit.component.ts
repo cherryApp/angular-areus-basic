adcd10d311284669faccc1186a56b5fa4cb3db3a
Joe Cserko <j.cserko@gmail.com>
2021-07-30 17:21:51 +0200

    feat(form): valueChanges

--- routing/src/app/page/user-edit/user-edit.component.ts
+++ routing/src/app/page/user-edit/user-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ValidatorFn, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { switchMap, tap } from 'rxjs/operators';
 import { FieldBase } from 'src/app/areus-form/model/field-base';
@@ -24,25 +25,51 @@ export class UserEditComponent implements OnInit {
   constructor(
     private ar: ActivatedRoute,
     private userService: UserService,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
     this.user$.subscribe(
       user => {
-        this.setForm(user);
         this.user = user;
+        this.setForm();
       }
     );
   }
 
-  setForm(user: User): void {
+  validateName(): ValidatorFn {
+    return control => {
+      control.valueChanges.subscribe(
+        val => {
+          if (val === 'showid') {
+            this.fields[0].type = 'text';
+          } else {
+            this.fields[0].type = 'hidden';
+          }
+        }
+      );
+      return null;
+    }
+  };
+
+  setForm(): void {
     this.fields = [
-      new InputField({key: 'firstName', label: 'First Name', type: 'text', value: user.firstName || ''})
+      new InputField({key: 'id', label: '', type: 'hidden', value: this.user?.id.toString() || '0',
+        validators: [Validators.required, Validators.minLength(5)]}),
+      new InputField({key: 'firstName', label: 'First Name', type: 'text', value: this.user?.firstName || '',
+        validators: [this.validateName(), Validators.required, Validators.minLength(5)]}),
+      new InputField({key: 'lastName', label: 'Last Name', type: 'text', value: this.user?.lastName || '',
+        validators: [Validators.required, Validators.minLength(5)]}),
+      new InputField({key: 'email', label: 'Email', type: 'email', value: this.user?.email || '',
+        validators: [Validators.required, Validators.email]}),
     ];
   }
 
   onSubmit(formValues: User): void {
-    console.log(formValues);
+    const updateData = {...formValues, id: this.user?.id || 0};
+    this.userService.update(updateData).subscribe(
+      user => this.router.navigate(['/', 'users'])
+    );
   }
 
 }
